f9549aae635cccd2de13f35066963b9592e25ffe
Anurag Lalotra <159163594+AnuragLalotra@users.noreply.github.com>
2025-11-04 12:26:45 +0530

    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String user = request.getParameter("username");
+        String pass = request.getParameter("password");
+        if ("admin".equals(user) && "1234".equals(pass)) {
+            out.println("<h2>Welcome, " + user + "!</h2>");
+        } else {
+            out.println("<h2>Invalid credentials. Try again.</h2>");
+        }
+    }
+}
